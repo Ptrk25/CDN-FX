b60715c77e0676c1a333df1269f399c4e7884aba
Ptrk25 <lbpatrick25@gmail.com>
2016-03-28 14:30:00 +0200

    OpenTicketDB updated

--- src/groovyfx/GroovyCIAController.java
+++ src/groovyfx/GroovyCIAController.java
@@ -2,6 +2,12 @@ package groovyfx;
 
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,7 +18,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebView;
 import javafx.stage.*;
 
 import java.awt.*;
@@ -59,6 +68,7 @@ public class GroovyCIAController implements Initializable {
             lblTitleCount,
             lblAttemptCount,
             lblFailedCount,
+            lblTitleName,
             lblTitleID,
             lblTMD,
             lblFilesCount,
@@ -76,7 +86,7 @@ public class GroovyCIAController implements Initializable {
 
     //Button
     @FXML
-    public static Button btnDownload;
+    Button btnDownload;
 
     //ToggledButton
     @FXML
@@ -109,11 +119,17 @@ public class GroovyCIAController implements Initializable {
     @FXML
     TableColumn<Ticket, Boolean> columnDL;
 
-    String dbpath, outputpath;
+    private String dbpath, outputpath;
+    private XMLHandler xml_handler = new XMLHandler(null);
 
     public void initialize(URL location, ResourceBundle resources){
         //DISABLE STUFF
-        //INIT PROPERTIES
+        xml_handler.getXMLFileFromServer();
+        initProperties();
+        addListener();
+    }
+
+    private void initProperties(){
         try{
             PropertiesHandler.createFile();
             if(PropertiesHandler.getInputPath() != null && PropertiesHandler.getInputPath().length() > 1){
@@ -123,6 +139,7 @@ public class GroovyCIAController implements Initializable {
             }
             if(PropertiesHandler.getOutputPath() != null && PropertiesHandler.getOutputPath().length() > 1){
                 outputpath = PropertiesHandler.getOutputPath();
+                menuRebuildRawContent.setDisable(false);
                 DebugLogger.log("INIT: Outputfolder setted", Level.INFO);
             }
         }catch (Exception e){
@@ -130,6 +147,15 @@ public class GroovyCIAController implements Initializable {
             e.printStackTrace(new PrintWriter(errors));
             DebugLogger.log(errors.toString(), Level.SEVERE);
         }
+    }
+
+    private void addListener(){
+        //Listview
+        listCategory.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                listCategory.getSelectionModel().getSelectedIndex(); //DO SOMETHING
+            }
+        });
     }
 
     private void updateCounters(TicketHandler th){
@@ -213,13 +239,80 @@ public class GroovyCIAController implements Initializable {
 
     private void openTicketDB() throws Exception{
         TicketHandler thandler = new TicketHandler();
+        ObservableList<Ticket> ticketlist;
+
         thandler.openFile(dbpath);
         thandler.addToTicketList();
         thandler.countTickets();
         thandler.sortTickets();
 
-        //UPDATE COUNTERS
+        //DATABASE
+        xml_handler.setTicketList(thandler.getTicketList());
+        ticketlist = xml_handler.readXMLFile();
+
         updateCounters(thandler);
+
+        tableTickets.setEditable(true);
+        tableTickets.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        //REMOVE THIS
+        tableTickets.getColumns().addListener(new ListChangeListener() {
+            @Override
+            public void onChanged(Change change) {
+                change.next();
+                if(change.wasReplaced()) {
+                    tableTickets.getColumns().clear();
+                    tableTickets.getColumns().addAll(columnName, columnRegion, columnSerial, columnType, columnTitleID, columnConsoleID, columnDL);
+                }
+            }
+        });
+
+        //TABLEFILTER
+        TableFilter.filteredTickets = new FilteredList<>(ticketlist, t -> true);
+        SortedList<Ticket> sortedTicket = new SortedList<>(TableFilter.createTableFilter(textSearch, tgbtnShowPreinstalledGame, listCategory));
+        sortedTicket.comparatorProperty().bind(tableTickets.comparatorProperty());
+        tableTickets.setItems(sortedTicket);
+
+        //TABLECOLUMNS
+        columnDL.setMinWidth(35.0);
+        columnDL.setMaxWidth(35.0);
+        columnConsoleID.setMinWidth(80.0);
+        columnConsoleID.setMaxWidth(80.0);
+        columnTitleID.setMinWidth(120.0);
+        columnTitleID.setMaxWidth(120.0);
+        columnType.setMinWidth(80.0);
+        columnType.setMaxWidth(80.0);
+        columnSerial.setMinWidth(80);
+        columnSerial.setMaxWidth(80);
+        columnRegion.setMinWidth(50);
+        columnRegion.setMaxWidth(50);
+
+        columnName.setCellValueFactory(
+                new PropertyValueFactory<Ticket, String>("Name")
+        );
+        columnRegion.setCellValueFactory(
+                new PropertyValueFactory<Ticket, String>("Region")
+        );
+        columnSerial.setCellValueFactory(
+                new PropertyValueFactory<Ticket, String>("Serial")
+        );
+        columnType.setCellValueFactory(
+                new PropertyValueFactory<Ticket, String>("Type")
+        );
+        columnTitleID.setCellValueFactory(
+                new PropertyValueFactory<Ticket, String>("TitleID")
+        );
+        columnConsoleID.setCellValueFactory(
+                new PropertyValueFactory<Ticket, String>("ConsoleID")
+        );
+        columnDL.setCellValueFactory(
+                new PropertyValueFactory<Ticket, Boolean>("DL")
+        );
+
+        columnName.setEditable(true);
+
+        textSearch.setDisable(false);
+        btnDownload.setDisable(false);
+        tgbtnShowPreinstalledGame.setDisable(false);
     }
 
     @FXML
@@ -233,6 +326,7 @@ public class GroovyCIAController implements Initializable {
         if(selectedFile != null){
             dbpath = selectedFile.getPath();
             openTicketDB();
+            DebugLogger.log("Ticket.db opened", Level.INFO);
         }
     }
 
@@ -243,6 +337,8 @@ public class GroovyCIAController implements Initializable {
         File selectedDirectory = directoryChooser.showDialog(mainPane.getScene().getWindow());
         if(selectedDirectory != null){
             outputpath = selectedDirectory.getPath();
+            menuRebuildRawContent.setDisable(false);
+            DebugLogger.log("Outputfolder setted", Level.INFO);
         }
     }
 
@@ -261,9 +357,12 @@ public class GroovyCIAController implements Initializable {
             stage.initStyle(StageStyle.UTILITY);
             stage.setTitle("Settings");
             stage.setScene(new Scene(root1));
+            stage.setResizable(false);
             stage.show();
         }catch (Exception e){
-
+            StringWriter errors = new StringWriter();
+            e.printStackTrace(new PrintWriter(errors));
+            DebugLogger.log(errors.toString(), Level.SEVERE);
         }
     }
 
@@ -273,13 +372,17 @@ public class GroovyCIAController implements Initializable {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../gui/RebuildCIA.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
+            stage.getIcons().add(new Image("/resources/gciaicon.png"));
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.initStyle(StageStyle.DECORATED);
             stage.setTitle("Rebuild raw content");
             stage.setScene(new Scene(root1));
+            stage.setResizable(false);
             stage.show();
         }catch (Exception e){
-            e.printStackTrace();
+            StringWriter errors = new StringWriter();
+            e.printStackTrace(new PrintWriter(errors));
+            DebugLogger.log(errors.toString(), Level.SEVERE);
         }
     }
 
@@ -290,16 +393,45 @@ public class GroovyCIAController implements Initializable {
         } catch (IOException e1) {
             e1.printStackTrace();
         } catch (URISyntaxException e1) {
-            e1.printStackTrace();
+            StringWriter errors = new StringWriter();
+            e1.printStackTrace(new PrintWriter(errors));
+            DebugLogger.log(errors.toString(), Level.SEVERE);
         }
     }
 
     @FXML
     protected void clickedAbout(){
         Alert about = new Alert(Alert.AlertType.INFORMATION);
+        Stage stage = (Stage)about.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("/resources/gciaicon.png"));
         about.setTitle("About");
         about.setHeaderText("Credits");
-        about.setContentText("GroovyCIA by Ptrk25\nFunkyCia2 by cearp\n\nTesters:\n\nduwen\nMadridi\nXenosaiga\nihaveamac\nCha0s Em3rald\n\nStarter XML by\n\nCha0s Em3rald\nMadridi\n\nVersion 0.91");
+        WebView webView = new WebView();
+        webView.getEngine().loadContent("<html>\n" +
+                "\t<body bgcolor=\"#F4F4F4\">\n" +
+                "\t\t<center>\n" +
+                "\t\t\t<font face=\"System\">\n" +
+                "\t\t\t\t<b>GroovyCIA</b> by Ptrk25<br>\n" +
+                "\t\t\t\t<b>FunkyCia2</b> by cearp<br><br>\n" +
+                "\t\t\t\t<b>Testers</b><br>\n" +
+                "\t\t\t\tduwen<br>\n" +
+                "\t\t\t\tMadridi<br>\n" +
+                "\t\t\t\tXenosaiga<br>\n" +
+                "\t\t\t\tihaveamac<br>\n" +
+                "\t\t\t\tCha0s Em3rald<br><br>\n" +
+                "\t\t\t\t<b>Community XML</b><br>\n" +
+                "\t\t\t\tCha0s Em3rald<br>\n" +
+                "\t\t\t\tMadridi<br>\n" +
+                "\t\t\t\tJimmsu<br><br>\n" +
+                "\t\t\t\t<b>Icon</b><br>\n" +
+                "\t\t\t\talirezay<br><br>\n" +
+                "\t\t\t\tVersion 0.91 Beta\n" +
+                "\t\t\t</font>\n" +
+                "\t\t</center>\n" +
+                "\t</body>\n" +
+                "</html>");
+        webView.setPrefSize(300,420);
+        about.getDialogPane().setContent(webView);
 
         about.showAndWait();
     }
@@ -327,6 +459,8 @@ public class GroovyCIAController implements Initializable {
     @FXML
     protected void selectedPersonal(){
         Alert warning = new Alert(Alert.AlertType.WARNING);
+        Stage stage = (Stage)warning.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("/resources/gciaicon.png"));
         warning.setTitle("Warning");
         warning.setHeaderText("Create personal CIAs");
         warning.setContentText("Don't use this if you want to install the CIAs on other systems!\n\nNOTE: Doesn't work with current Firmwares/CFW, CIA won't be installable!");
